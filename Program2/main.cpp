2c5267f7da049ad360b6cc7fdab9961f04ab7c3f
Tyler Schmitt <tylerschmitt.se@gmail.com>
2023-07-29 16:04:04 -0400

    Ingest key from Program2

--- Program2/main.cpp
+++ Program2/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cxxopts.hpp>
+#include <optional>
 
-int main() {
-    std::cout << "Hello, Program 2!" << std::endl;
-    return 0;
+std::optional<std::string> ParseArgs(int argc, char *argv[]) {
+    cxxopts::Options options("Program1", "Encrypt and send message to Program2.");
+
+    options.add_options()
+    ("k,key", "Key to encrypt message", cxxopts::value<std::string>())
+    ("m,message", "Message to encrypt", cxxopts::value<std::string>())
+    ;
+
+    auto result = options.parse(argc, argv);
+
+    std::optional<std::string> key = std::nullopt;
+    if (result.count("key")) {
+      key = result["key"].as<std::string>();
+    } else {
+        std::cerr << "No key argument!" << std::endl;
+    }
+
+    return key;
+}
+
+int main(int argc, char *argv[]) {
+    const auto key = ParseArgs(argc, argv);
+
+    if (key.has_value()) {
+        std::cout << key.value() << std::endl;
+        return 0;
+    } else {
+        std::cerr << "Invalid arguments." << std::endl;
+        return EXIT_FAILURE;
+    }
 }
