c8d5f0b9b612f4cdbf6449c8bab162c524009cc0
Tyler Schmitt <tylerschmitt.se@gmail.com>
2023-07-30 15:00:31 -0400

    Update cmake structure, add test

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 if [ $# -gt 0 ] && [ $1 -eq 1 ]
 then
-    echo "Clearing build directory..."
+    echo "\nClearing build directory..."
     rm -rf build
     mkdir build
 fi
 
-echo "Building..."
+echo "\nBuilding..."
 
 cd build
 cmake ..
 make
 
-echo "Running example..."
+echo "\nRunning tests..."
+./spideroak_test crypto_test
 
-./src/Program2/Program2 -k "40baaed112a2fdc0934055fd625f906a4a21a07c6aaf45de691b4f6962f10b88" &
+echo "\nRunning example..."
+
+./Program2 -k "40baaed112a2fdc0934055fd625f906a4a21a07c6aaf45de691b4f6962f10b88" &
 
 sleep 1
 
-./src/Program1/Program1 -k "40baaed112a2fdc0934055fd625f906a4a21a07c6aaf45de691b4f6962f10b88" -m "Message to encrypt"
+./Program1 -k "40baaed112a2fdc0934055fd625f906a4a21a07c6aaf45de691b4f6962f10b88" -m "Message to encrypt"
 
 sleep 1
 
-echo "Done!"
+echo "\nDone!"
 
 exit 0
