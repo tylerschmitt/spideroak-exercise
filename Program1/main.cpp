049570ca09790003e9532fd5f0c4f7bef599251c
Tyler Schmitt <tylerschmitt.se@gmail.com>
2023-07-30 08:54:52 -0400

    Message -> plaintext, CMake openssl

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
+
 #include <cxxopts.hpp>
+#include <iostream>
 #include <optional>
 
-struct KeyAndMessage {
+
+struct KeyAndPlaintext {
     std::optional<std::string> key;
-    std::optional<std::string> message;
+    std::optional<std::string> plaintext;
 };
 
-KeyAndMessage ParseArgs(int argc, char *argv[]) {
+
+
+KeyAndPlaintext ParseArgs(int argc, char *argv[]) {
     cxxopts::Options options("Program1", "Encrypt and send message to Program2.");
 
     options.add_options()
@@ -24,22 +28,28 @@ KeyAndMessage ParseArgs(int argc, char *argv[]) {
         std::cerr << "No key argument!" << std::endl;
     }
 
-    std::optional<std::string> message = std::nullopt;
+    std::optional<std::string> plaintext = std::nullopt;
     if (result.count("message")) {
-      message = result["message"].as<std::string>();
+      plaintext = result["message"].as<std::string>();
     } else {
         std::cerr << "No message argument!" << std::endl;
     }
 
-    return KeyAndMessage{key, message};
+    return KeyAndPlaintext{key, plaintext};
+}
+
+// Adapted from: https://stackoverflow.com/questions/73631293/how-to-encrypt-a-string-using-openssl-c-library-and-a-public-key-file
+std::string EncryptMessage(const KeyAndPlaintext key_msg) {
+    return ""; // add exception/error handling
 }
 
 int main(int argc, char *argv[]) {
-    const auto key_and_message = ParseArgs(argc, argv);
+    const auto key_and_plaintext = ParseArgs(argc, argv);
 
-    if (key_and_message.key.has_value() && key_and_message.message.has_value()) {
-        std::cout << key_and_message.key.value() << std::endl;
-        std::cout << key_and_message.message.value() << std::endl;
+    if (key_and_plaintext.key.has_value() && key_and_plaintext.plaintext.has_value()) {
+
+        const auto encrypted_msg = EncryptMessage(key_and_plaintext);
+
         return 0;
     } else {
         std::cerr << "Invalid arguments." << std::endl;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spideroak-exercise
+
+This project uses OpenSSL, you may need to install via `brew install openssl` (or some similar command depending on your OS).
