9fadbfd7663259714cb139237c37edef3884fb36
Tyler Schmitt <tylerschmitt.se@gmail.com>
2023-07-30 10:00:04 -0400

    Add link to citation

--- crypto/crypto.hpp
+++ crypto/crypto.hpp
@@ -1,4 +1,5 @@
 
 namespace spideroak_crypto {
+    // Adapted from: https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
     int encrypt(unsigned char * key, unsigned char * plaintext, size_t plaintext_len, unsigned char *ciphertext);
 }
